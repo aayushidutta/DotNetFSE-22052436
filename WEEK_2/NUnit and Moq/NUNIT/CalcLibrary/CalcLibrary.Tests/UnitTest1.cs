7abdfe12f21176277ae72f13e73c00879b026f8c
aayushidutta <adutta02112004@gmail.com>
2025-06-29 17:48:27 +0530

    Add Advanced SQL Files in Week_2

---
+++ WEEK_2/NUnit and Moq/NUNIT/CalcLibrary/CalcLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [Test]
+        [TestCase(2, 3, 5)]
+        [TestCase(10, 15, 25)]
+        [TestCase(-5, 5, 0)]
+        public void Add_ReturnsCorrectSum(int a, int b, int expected)
+        {
+            int result = calc.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            
+        }
+    }
+}
